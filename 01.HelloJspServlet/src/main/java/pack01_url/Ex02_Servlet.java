c561fcd99044de0da5e7d145db6c505a4ebe5f4f
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-11-09 17:55:57 +0900

    .

---
+++ 01.HelloJspServlet/src/main/java/pack01_url/Ex02_Servlet.java
@@ -0,0 +1,25 @@
+package pack01_url;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Ex02_Servlet")
+public class Ex02_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+        
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("HelloServlet"); //.append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
